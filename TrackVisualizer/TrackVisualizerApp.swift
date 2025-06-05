8c7b2d0a2781e9519beeb941fcf907d2178b6686
Adnar Lozano <adnarlozano@gmail.com>
2025-06-05 16:46:23 -0700

    Initial Commit

---
+++ TrackVisualizer/TrackVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackVisualizerApp.swift
+//  TrackVisualizer
+//
+//  Created by hostname on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
