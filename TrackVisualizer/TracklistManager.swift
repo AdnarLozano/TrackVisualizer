d28dac7f1950197b85235f107dc888949fe43a41
Adnar Lozano <adnarlozano@gmail.com>
2025-06-05 20:00:04 -0700

    Add persistence for tracklist using UserDefaults

--- TrackVisualizer/TracklistManager.swift
+++ TrackVisualizer/TracklistManager.swift
@@ -1,5 +1,27 @@
 import Foundation
 
 class TracklistManager: ObservableObject {
-    @Published var tracks: [URL] = []
+    @Published var tracks: [URL] = [] {
+        didSet {
+            saveTracks()
+        }
+    }
+
+    private let defaults = UserDefaults.standard
+    private let tracksKey = "TrackVisualizerTracks"
+
+    init() {
+        loadTracks()
+    }
+
+    private func saveTracks() {
+        let urlsAsStrings = tracks.map { $0.absoluteString }
+        defaults.set(urlsAsStrings, forKey: tracksKey)
+    }
+
+    private func loadTracks() {
+        if let urlsAsStrings = defaults.array(forKey: tracksKey) as? [String] {
+            tracks = urlsAsStrings.compactMap { URL(string: $0) }
+        }
+    }
 }
